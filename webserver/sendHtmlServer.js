1c2897bcd9adb1342dbc8bbf158dd6055dc1c0fa
mmtos <js223kr92@naver.com>
2020-05-31 00:00:14 +0900

    add example(send html file)

--- webserver/sendHtmlServer.js
+++ webserver/sendHtmlServer.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer( (req,res) => {
-	res.write('<h1>Hello</h1>')
-	res.end('<p>nodeJs world</p>')
+	fs.readFile('./sendHtml.html',(err,data) =>{
+		if(err){throw err;}
+		res.end(data);
+	});
 });
 
 server.listen(8080, () =>{
